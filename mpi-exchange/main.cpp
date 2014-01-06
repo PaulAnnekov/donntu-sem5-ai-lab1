2434b01af77f8086f834b5449640027aeee241bf
Paul Annekov <paul.annekov@gmail.com>
2014-01-07 01:56:17 +0200

    Input data parse

--- mpi-exchange/main.cpp
+++ mpi-exchange/main.cpp
@@ -1,17 +1,28 @@
 #include <cstdlib>
 #include <cstdio>
 #include <mpich2/mpi.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
 
 int main(int argc, char** argv) {
-    int rank, size;
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
- 
-    printf( "Hello world from process %d of %d\n", rank, size );
- 
-    MPI_Finalize();
+    if (argc != 2)
+        return 0;
     
+    std::string data (argv[1]);
+    std::vector<float> vectors[2];
+    int current=0;
+    
+    std::stringstream ss1(data);
+    std::string vector, number;
+    while (std::getline(ss1, vector, ';') && current < 2) {
+        std::stringstream ss2(vector);
+        while (std::getline(ss2, number, ',')) {
+            vectors[current].push_back(atof(number.c_str()));
+        }
+        current++;
+    }
+
     return 0;
 }
