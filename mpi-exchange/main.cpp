f0a7a3325dafa7ecb61d0bdacebb6bc0629fbae1
Paul Annekov <paul.annekov@gmail.com>
2014-01-10 21:42:43 +0200

    Input parse.

--- mpi-exchange/main.cpp
+++ mpi-exchange/main.cpp
@@ -1,28 +1,42 @@
+#include "main.hpp"
+
 #include <cstdlib>
 #include <cstdio>
 #include <mpich2/mpi.h>
 #include <algorithm>
-#include <vector>
 #include <string>
 #include <sstream>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, char** argv) {
     if (argc != 2)
         return 0;
-    
-    std::string data (argv[1]);
-    std::vector<float> vectors[2];
-    int current=0;
-    
-    std::stringstream ss1(data);
-    std::string vector, number;
-    while (std::getline(ss1, vector, ';') && current < 2) {
-        std::stringstream ss2(vector);
-        while (std::getline(ss2, number, ',')) {
-            vectors[current].push_back(atof(number.c_str()));
-        }
-        current++;
-    }
 
+    int rank;
+    vector< vector<float> > vectors;
+    parse_input(argv[1],vectors);
+    
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    
+    MPI_Finalize();
+    
     return 0;
+}
+
+void parse_input(const char* string_vectors, vector< vector<float> > &vectors) {
+    string data(string_vectors);
+
+    stringstream ss1(data);
+    string string_vector, number;
+    while (getline(ss1, string_vector, ';')) {
+        stringstream ss2(string_vector);
+        vectors.push_back(vector<float>());
+        while (getline(ss2, number, ',')) {
+            vectors[vectors.size()-1].push_back(atof(number.c_str()));
+        }
+    }
 }
---
+++ mpi-exchange/main.hpp
@@ -0,0 +1,9 @@
+#ifndef MAIN_HPP
+#define	MAIN_HPP
+
+#include <vector>
+
+void parse_input(const char* string_vectors, std::vector< std::vector<float> > &vectors);
+
+#endif	/* MAIN_HPP */
+
