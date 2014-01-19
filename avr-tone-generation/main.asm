66df03614933e651ec805e4a20fe88512845f4ef
Paul Annekov <paul.annekov@gmail.com>
2014-01-19 22:44:37 +0200

    Moved some values to constants.

--- avr-tone-generation/main.asm
+++ avr-tone-generation/main.asm
@@ -7,6 +7,12 @@
 	.endif
 .endm
 
+.equ 	Timer_Compare = 156 ; 8*10^6 Hz / 64 / 156 = 800 Hz.
+.equ 	Max_Interrupts = 1600 ; 2 seconds (800 * 2 = 1600 = 0x0640).
+.equ 	Toggled_Pin = PINB
+.equ 	Toggled_Bit = PINB6
+.equ	TCCR0B_State = 0b00000011 ; 011 Timer clock (Timer0) = system clock / 64.
+
 ; Data segment.
 .dseg
 Counter:	.byte	2
@@ -32,8 +38,8 @@ uout SPL,R16
 ldi R16,High(RAMEND)
 uout SPH,R16
 
-; 011 Timer clock (Timer0) = system clock / 64.
-ldi r16, (1<<CS00)|(1<<CS01)			
+; Set TCCR0B control register state.
+ldi r16, TCCR0B_State			
 uout TCCR0B, r16						
 
 ; Clear TOV0/ clear pending interrupts.
@@ -45,13 +51,13 @@ ldi r16, 1<<OCIE0A
 uout TIMSK0, r16						
 
 ; Write value to compare timer to.
-ldi r16, 156							
+ldi r16, Timer_Compare							
 out OCR0A, r16							
 
-; Initialize tone duration counter to 2 seconds (800 * 2 = 0x0640).
-ldi r16, 0x40									
+; Initialize tone duration counter.
+ldi r16, low(Max_Interrupts)									
 sts Counter, r16
-ldi r16, 0x06
+ldi r16, high(Max_Interrupts)
 sts Counter+1, r16
 
 ; Enable interrupts globally.
@@ -67,14 +73,14 @@ rjmp Wait
 ; Handle TimerCounter0 Compare Match A Interrupt.
 Timer:
 ; Toggle PORTB 6 bit: "Writing a logic one to PINxn toggles the value of PORTxn, independent on the value of DDRxn".									
-ldi r16, (1<<PINB6) 
-uout PINB, r16
+ldi r16, (1<<Toggled_Bit) 
+uout Toggled_Pin, r16
 
 ; Decrement our Counter.
 lds r16, Counter
 lds r17, Counter+1
-subi	R16,1			; Substract 1.
-sbci	R17,0			; Substract 1 only if С flag raised.
+subi R16,1			; Substract 1.
+sbci R17,0			; Substract 1 only if С flag raised.
 brmi Disable_Timer		; Disable timer if previous operation results in negative value.
 sts Counter, r16
 sts Counter+1, r17
