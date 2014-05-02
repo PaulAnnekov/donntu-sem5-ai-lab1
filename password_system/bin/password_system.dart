05a06c6375c49c7a2a29b002ed6399728ee2eed4
Paul Annekov <paul.annekov@gmail.com>
2014-05-03 00:24:53 +0300

    password_system: Password hashing, file encoding with AES, etc.

--- password_system/bin/password_system.dart
+++ password_system/bin/password_system.dart
@@ -1,5 +1,36 @@
 import "../lib/db.dart";
+import "dart:io";
+
+Db db;
 
 void main() {
-  Db db = new Db();
+  db = new Db("key");
+
+  stdout.writeln("Hi");
+
+  var is_exit=false;
+  while (!is_exit) {
+    stdout.writeln("Write actions");
+
+    var action = stdin.readLineSync();
+    var parts = action.split(' ');
+
+    switch(parts[0]) {
+      case 'add':
+        db.addUser(parts[1], parts[2]);
+        stdout.writeln('User "${parts[1]}" was added');
+        break;
+      case 'get':
+        stdout.writeln(db.getPassword(parts[1]));
+        break;
+      case 'get_all':
+        stdout.writeln(db.getLoginData());
+        break;
+      case 'exit':
+        is_exit=true;
+        break;
+    }
+  }
+
+  stdout.writeln("GL HF");
 }
--- password_system/lib/db.dart
+++ password_system/lib/db.dart
@@ -1,19 +1,103 @@
-import "package:cipher/block/aes_fast.dart";
+import "dart:typed_data";
+import "dart:io";
+import "dart:convert";
+
+import "package:cipher/cipher.dart";
+import "package:cipher/impl/server.dart";
 
 class Db {
-  Db() {
+  List<int> _key;
+  String _path = "./db.data";
+  List<int> _salt = [];
+  Map<String, String> _db = null;
 
+  Db(String masterPassword, [String file]) {
+    initCipher();
+
+    _key=_genKey(masterPassword);
+
+    if (file != null) {
+      this._path = file;
+    }
+
+    _initDb();
   }
 
-  addUser() {
+  /**
+   * Generates DB key from password.
+   */
+  _genKey(String password) {
+    var salt = new Uint8List.fromList(_salt);
+    var params = new Pbkdf2Parameters(salt, 100, 16);
+    var keyDerivator = new KeyDerivator("SHA-1/HMAC/PBKDF2")
+      ..init(params)
+    ;
 
+    return keyDerivator.process(UTF8.encode(password));
   }
 
-  getUser() {
+  /**
+   * Gets database from file.
+   */
+  Object _initDb() {
+    var file = new File(_path);
 
+    if (!file.existsSync()) {
+      _db = {};
+      return _db;
+    }
+
+    var params = new KeyParameter(_key);
+    var cipher = new BlockCipher("AES")..init(false, params);
+
+    var cipherText = cipher.process(file.readAsBytesSync());
+
+    _db=JSON.decode(UTF8.decode(cipherText));
+
+    return _db;
   }
 
-  isExists() {
+  /**
+   * Encodes data and writes it to database file.
+   */
+  _saveDb() {
+    var file = new File(_path);
 
+    var params = new KeyParameter(_key);
+    var cipher = new BlockCipher("AES")..init(true, params);
+
+    var cipherText = cipher.process(UTF8.encode(JSON.encode(_db)));
+
+    file.writeAsBytesSync(cipherText);
+  }
+
+  /**
+   * Adds new user with [login] and [password] to DB.
+   */
+  void addUser(String login, String password) {
+    var digest = new Digest('SHA-256');
+    var hash = digest.process(UTF8.encode(password));
+
+    _db[login]=hash;
+    _saveDb();
+  }
+
+  /**
+   * Gets user's hashed password by [login].
+   * Returns null if the passed login does not exist.
+   */
+  String getPassword(String login) {
+    if (!_db.containsKey(login)) {
+      return null;
+    }
+
+    return _db["login"];
+  }
+
+  /**
+   * Gets all users and hashed passwords from DB as formatted string.
+   */
+  String getLoginData() {
+    return _db.toString();
   }
 }
